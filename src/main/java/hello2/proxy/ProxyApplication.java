4ffec87896f4989b87989a49c65133d59bfa9a50
hyungkiShin <tkaqkeldk@nate.com>
2022-03-08 14:02:19 +0900

    [Components Scan] V1 V2 작성

--- src/main/java/hello2/proxy/ProxyApplication.java
+++ src/main/java/hello2/proxy/ProxyApplication.java
@@ -1,8 +1,12 @@
 package hello2.proxy;
 
+import hello2.proxy.config.AppV1Config;
+import hello2.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import({AppV1Config.class,AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello2.proxy.app")
 public class ProxyApplication {
 
