a20a9602ffcc59008101e28bf99ff9f7ee88133d
hyungkiShin <tkaqkeldk@nate.com>
2022-03-10 14:06:43 +0900

    [구체 클래스 기반]
    인터페이스가 없어도 상속 기반 Proxy 생성

--- src/test/java/hello2/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
+++ src/test/java/hello2/proxy/pureproxy/concreteproxy/ConcreteProxyTest.java
@@ -2,6 +2,7 @@ package hello2.proxy.pureproxy.concreteproxy;
 
 import hello2.proxy.pureproxy.concreteproxy.code.ConcreteClient;
 import hello2.proxy.pureproxy.concreteproxy.code.ConcreteLogic;
+import hello2.proxy.pureproxy.concreteproxy.code.TimeProxy;
 import org.junit.jupiter.api.Test;
 
 public class ConcreteProxyTest {
@@ -10,6 +11,14 @@ public class ConcreteProxyTest {
     void noProxy() {
         ConcreteLogic concreteLogic = new ConcreteLogic();
         ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.excute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
         client.excute();
     }
 }
